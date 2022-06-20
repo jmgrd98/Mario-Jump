e29545539d64e10e09f8fd555bb3cbbb5bad5963
João Marcelo Dantas <jmgrd98@gmail.com>
2022-06-20 13:32:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,24 @@ const jump = () => {
 const loop = setInterval(() => {
     
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px');
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    // Game Over
 
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src='game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
     }
 
 }, 10);
